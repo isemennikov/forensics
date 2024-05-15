624966c4334537ebea82632f0787e0c108d6675d
Ilay Semennikov <semennikovia@gmail.com>
2024-05-15 12:13:47 +0300

    format raw str3

--- DetectAV_for_Win/DetectAntivirusService.py
+++ DetectAV_for_Win/DetectAntivirusService.py
@@ -5,7 +5,7 @@ def check_auto_start_services(av_list_file):
         av_list_file = [line.strip() for line in file if line.strip()]
 
         regheve = winreg.HKEY_LOCAL_MACHINE
-        regpath = "SYSTEM\CurrentControlSet\Services"
+        regpath = r"SYSTEM\CurrentControlSet\Services"
 
         try:
             with winreg.OpenKey(regheve, regpath, access=winreg.KEY_READ) as key:
@@ -14,8 +14,8 @@ def check_auto_start_services(av_list_file):
                 for i in range(num_keys):
                     try:
                         subkey_name = winreg.EnumKey(key, i)
-                        if any(av_name in subkey_name for av_name in av_list):
-                            sub_path = fr"{regpath}{subkey_name}"
+                        if any(av_name in subkey_name for av_name in av_list_file):
+                            sub_path = fr"{regpath}\{subkey_name}"
                             with winreg.OpenKey(regheve, sub_path, access=winreg.KEY_READ) as subkey:
                                 start_type = winreg.QueryValueEx(subkey, 'Start')[0]
                                 if start_type == 2:
