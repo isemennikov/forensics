119b5e7fe3968941a5056a334213caef536af3cb
Ilay Semennikov <semennikovia@gmail.com>
2024-05-15 18:45:26 +0300

    trableshooting  ref print and logical 1

--- Reconnaissance_and_Resource_Development/HonyScan.py
+++ Reconnaissance_and_Resource_Development/HonyScan.py
@@ -24,19 +24,37 @@ def analyze_packets(packet):
     source = packet[IP].src if packet.haslayer(IP) else packet[IPv6].src
     port = packet[TCP].dport
 
+    # Проверяем, исходит ли пакет от заблокированного хоста
     if source in blocked_hosts:
-        flags = "RA" if port in open_ports else ("SA" if port in honey_ports else None)
+        # Если пакет пришел на открытый порт, отправляем флаги RST и ACK
+        if port in open_ports:
+            flags = "RA"
+            print(f"Sending RST+ACK in response to traffic from {source} on open port {port}")
+        # Если пакет пришел на "медовый" порт, отправляем флаги SYN и ACK
+        elif port in honey_ports:
+            flags = "SA"
+            print(f"Sending SYN+ACK in response to traffic from {source} on honey port {port}")
+        else:
+            # Если пакет пришел на порт, который не является открытым или "медовым", не отправляем ответ
+            return
     else:
-        if port not in open_ports + honey_ports:
+        # Если исходящий хост не заблокирован, проверяем порт
+        if port not in open_ports and port not in honey_ports:
+            # Порт не открыт и не "медовый", добавляем хост в заблокированные
             blocked_hosts.append(source)
-            flags = "SA" if port in honey_ports else None
+            print(f"Adding {source} to blocked hosts, no response sent")
+            return
+        elif port in honey_ports:
+            # Порт "медовый" и хост не заблокирован, отправляем флаги SYN и ACK
+            flags = "SA"
+            print(f"Sending SYN+ACK in response to traffic from {source} on honey port {port}")
+        else:
+            # Пакет пришел на открытый порт, не отправляем ответ
+            return
 
-    if flags:
-        response = create_response(packet, flags)
-        sendp(response, verbose=False)
-        if flags == "RA":
-            print(f"Sending {flags} packet in response to traffic from {source} on port {port}")
-
+    # Создаем и отправляем ответный пакет
+    response = create_response(packet, flags)
+    sendp(response, verbose=False)
 
 filter_rule = f"dst host {ip_address} and tcp"
 sniff(filter=filter_rule, prn=analyze_packets)
