936bfaac8e1e59e402eae8b5e387880ae73c2699
Ilay Semennikov <semennikovia@gmail.com>
2024-05-15 13:21:13 +0300

    ref tests

--- DetectAV_for_Win/tests/pytest_DetectAVServices.py
+++ DetectAV_for_Win/tests/pytest_DetectAVServices.py
@@ -1,13 +1,14 @@
 import pytest
 from unittest.mock import Mock, patch
-from DetectAntivirusServisec.py  import get_auto_start_services  # Импортируйте вашу функцию
+from DetectAntivirusService  import check_auto_start_services  # Импортируйте вашу функцию
 #
 
 def test_get_auto_start_services_empty():
     # Тест на отсутствие служб в списке
     with patch('builtins.open', pytest.mock.mock_open(read_data="")):
-        assert get_auto_start_services("av_services.txt") == []
+        assert check_auto_start_services("av_services.txt") == []
 
+@patch('DetectAntivirusService.winreg')
 @patch('winreg.OpenKey')
 @patch('winreg.QueryInfoKey', return_value=(1, 0))
 @patch('winreg.EnumKey', return_value='SomeAntivirusService')
@@ -15,7 +16,7 @@ def test_get_auto_start_services_empty():
 def test_get_auto_start_services_found(mock_openkey, mock_queryinfokey, mock_enumkey, mock_queryvalueex):
     # Тест на наличие службы с автозапуском в списке
     with patch('builtins.open', pytest.mock.mock_open(read_data="SomeAntivirusService")):
-        services = get_auto_start_services("av_services.txt")
+        services = check_auto_start_services("av_services.txt")
         assert services == ['SomeAntivirusService']
         mock_openkey.assert_called_with(winreg.HKEY_LOCAL_MACHINE, r"SYSTEMCurrentControlSetServicesSomeAntivirusService", access=winreg.KEY_READ)
 
