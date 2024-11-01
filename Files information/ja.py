1f86c80d6c76c897477930d641c9f02d10e667fc
Ilay Semennikov <semennikovia@gmail.com>
2024-11-01 14:29:59 +0300

    cosmetoc change

--- Files information/ja.py
+++ Files information/ja.py
@@ -20,7 +20,7 @@ parsers.add_argument("OUTPUT_FILE", help="Path to output file")
 
 # Optional Arguments
 parsers.add_argument("--hash", help="Hash the files", action="store_true")
-parsers.add_argument("--hash-algoritm", help="Hash algorithm to use: SHA1 or/and SHA256", choices=['SHA1', 'SHA256'],
+parsers.add_argument("--hash-algoritm", help="Hash algorithm to use: SHA1 or SHA256", choices=['SHA1', 'SHA256'],
                      default="SHA256")
 parsers.add_argument("-l", "--log", help="Path to log file", required=True)
 
@@ -30,22 +30,22 @@ args = parsers.parse_args()
 input_path = args.INPUT_FILE
 output_file = args.OUTPUT_FILE
 
-# Логирование начала обработки
+# Logging the start of parsing
 with open(args.log, 'a') as log_file:
     if os.path.isdir(input_path):
-        log_file.write(f"[{datetime.now()}] Начало обработки директории {input_path}\n")
+        log_file.write(f"[{datetime.now()}] Start parsing directory {input_path}\n")
 
-        # Рекурсивная обработка файлов в указанной директории
+        # Recursively process files in the set directory
         for root, dirs, files in os.walk(input_path):
             for file in files:
                 full_input_path = os.path.join(root, file)
-                log_file.write(f"[{datetime.now()}] Обработка файла {full_input_path}\n")
+                log_file.write(f"[{datetime.now()}] Parsing the file {full_input_path}\n")
 
                 if args.hash:
                     ha = args.hash_algoritm
                     print(f"File hashing enabled with {ha} algorithm for {full_input_path}")
 
-                    # Хеширование файла
+                    # Hashing the file
                     hash_func = hashlib.new(ha)
                     try:
                         with open(full_input_path, 'rb') as f:
@@ -53,25 +53,25 @@ with open(args.log, 'a') as log_file:
                                 hash_func.update(chunk)
                         hash_value = hash_func.hexdigest()
 
-                        # Запись хеш-суммы в выходной файл
-                        with open(output_file, 'a') as out_file:  # Используем 'a' для добавления
+                        # Write hash sum to output file
+                        with open(output_file, 'a') as out_file:  # Use 'a' for adding
                             out_file.write(f"Hash: {hash_value}\n")
 
-                        # Логирование успешного завершения
+                        # Logging successful completion
                         log_file.write(
-                            f"[{datetime.now()}] Обработка завершена успешно. Результат записан в {output_file}\n")
+                            f"[{datetime.now()}] Processing completed successfully. The result is written to {output_file} file\n")
                     except Exception as e:
-                        log_file.write(f"[{datetime.now()}] Ошибка при обработке файла {full_input_path}: {e}\n")
-                        print(f"Ошибка при обработке файла {full_input_path}: {e}")
+                        log_file.write(f"[{datetime.now()}] Erro parsing {full_input_path} file: {e}\n")
+                        print(f"Error parsing  {full_input_path} file: {e}")
     else:
-        # Обработка одного файла
-        log_file.write(f"[{datetime.now()}] Начало обработки файла {input_path}\n")
+        # Parsing one file
+        log_file.write(f"[{datetime.now()}] Start parsing {input_path} file\n")
 
         if args.hash:
             ha = args.hash_algoritm
             print(f"File hashing enabled with {ha} algorithm for {input_path}")
 
-            # Хеширование файла
+            # Hashing the file
             hash_func = hashlib.new(ha)
             try:
                 with open(input_path, 'rb') as f:
@@ -79,14 +79,14 @@ with open(args.log, 'a') as log_file:
                         hash_func.update(chunk)
                 hash_value = hash_func.hexdigest()
 
-                # Запись хеш-суммы в выходной файл
+                # Write hash sum to output file
                 with open(output_file, 'w') as out_file:
                     out_file.write(f"Hash: {hash_value}\n")
 
-                # Логирование успешного завершения
-                log_file.write(f"[{datetime.now()}] Обработка завершена успешно. Результат записан в {output_file}\n")
+                # Logging successful completion
+                log_file.write(f"[{datetime.now()}] Processing completed successfully. The result is written to {output_file} file\n")
             except Exception as e:
-                log_file.write(f"[{datetime.now()}] Ошибка при обработке файла {input_path}: {e}\n")
-                print(f"Ошибка при обработке файла {input_path}: {e}")
+                log_file.write(f"[{datetime.now()}] Error parsing {input_path} file: {e}\n")
+                print(f"Erros parsing {input_path} file: {e}")
         else:
-            log_file.write(f"[{datetime.now()}] Хеширование не включено.\n")
+            log_file.write(f"[{datetime.now()}] Hasing not set.\n")
